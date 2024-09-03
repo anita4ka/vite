35a0a88f443c656d59e83e2d67b46d8cad2ddf3e
anita4ka <annshund@gmail.com>
2024-09-03 15:24:23 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import Item from './item.tsx'
+import { Task } from './types.ts';
 
 function App() {
-  const initialTodos: string [] = [
-    "wakeup",
-    "homework",
-    "eat",
-    "sleep",
+  const initialTodos: Task[] = [
+   {id: "0", title: "wakeup",},
+  {id: "1", title: "homework",},
+   {id: "2", title: "eat",},
+   {id: "3", title: "sleep",},
   ];
 
   const [todos, setTodos] = useState(
     JSON.parse(localStorage.getItem("todos") ?? JSON.stringify(initialTodos)) 
   );
-  const addTodoHandler = (e) => setTodos([...todos, e.target.value]);
+  const addTodoHandler = (e) => {
+   
+    setTodos([...todos, inputRef.current.value])
+  
+  };
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
   const inputRef = useRef<any>(null);
-
+  const deleteTodoHandler = (id: string) => {
+    
+    const remainingTodo = todos.filter((item: Task) => item.id !== id);
+    setTodos(remainingTodo);
+  };
   return (
   
       <div>
-     {todos.map((item: any, index: any) => {
-        return <Item key={index} item={item} />
-      })};
+     {todos.map((item: Task) => {
+        return <Item key={item.id} item={item} deleteTodo={deleteTodoHandler}/>
+      })}
 
        <form>
-        <input onChange={(e) => (inputRef.current as unknown as string) = e.target.value} type='text' placeholder='Ввести задачу'/>
+        <input ref={inputRef} type='text' placeholder='Ввести задачу'/>
         <button onClick={addTodoHandler} type='submit'>
           Добавить
         </button>
--- src/item.tsx
+++ src/item.tsx
@@ -1,5 +1,18 @@
-const Item = ({ item }: { item: string }) => {
-    return <div>{item}</div>;
-  };
-  
+import { Task } from "./types";
+
+const Item = ({
+  item,
+  deleteTodo,
+}: {
+  item: Task;
+  deleteTodo: (id: string) => void;
+}) => {
+  console.log(item);
+  return (
+    <>
+      <div>{item.title}</div>
+      <button onClick={() => deleteTodo(item.id)}>Удалить</button>
+    </>
+  );
+};
   export default Item;
